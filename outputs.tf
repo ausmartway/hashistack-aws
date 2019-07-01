4fe088e65491220a3298d40a6b9022f8ebff28d3
Yulei Liu <yulei.liu@gmail.com>
2019-07-01 21:54:51 +1000

    upgraded to 0.12

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,12 @@ You can now interact with Consul using any of the CLI
 (https://www.consul.io/docs/commands/index.html) or
 API (https://www.consul.io/api/index.html) commands.
 
-${format("Consul UI: %s %s\n\n%s", module.consul_lb_aws.consul_lb_dns, var.public ? "(Public)" : "(Internal)", var.public ? "The Consul nodes are in a public subnet with UI & SSH access open from the\ninternet. WARNING - DO NOT DO THIS IN PRODUCTION!" : "The Consul node(s) are in a private subnet, UI access can only be achieved inside\nthe network through a VPN.")}
+${format(
+"Consul UI: %s %s\n\n%s",
+module.consul_lb_aws.consul_lb_dns,
+var.public ? "(Public)" : "(Internal)",
+var.public ? "The Consul nodes are in a public subnet with UI & SSH access open from the\ninternet. WARNING - DO NOT DO THIS IN PRODUCTION!" : "The Consul node(s) are in a private subnet, UI access can only be achieved inside\nthe network through a VPN.",
+)}
 
 Use the CLI to retrieve the Consul members, write a key/value, and read
 that key/value.
@@ -20,45 +25,7 @@ that key/value.
 Use the HTTP API to retrieve the Consul members, write a key/value,
 and read that key/value.
 
-${!var.use_lb_cert ?
-"If you're making HTTP API requests to Consul from the Bastion host,
-the below env var has been set for you.
-
-  $ export CONSUL_ADDR=http://127.0.0.1:8500
-
-  $ curl \\
-      -X GET \\
-      $${CONSUL_ADDR}/v1/agent/members | jq '.' # Retrieve Consul members
-  $ curl \\
-      -X PUT \\
-      -d '{\"bar=baz\"}' \\
-      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Write a KV
-  $ curl \\
-      -X GET \\
-      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Read a KV"
-:
-"If you're making HTTPS API requests to Consul from the Bastion host,
-the below env vars have been set for you.
-
-  $ export CONSUL_ADDR=https://127.0.0.1:8080
-  $ export CONSUL_CACERT=/opt/consul/tls/consul-ca.crt
-  $ export CONSUL_CLIENT_CERT=/opt/consul/tls/consul.crt
-  $ export CONSUL_CLIENT_KEY=/opt/consul/tls/consul.key
-
-  $ curl \\
-      -X GET \\
-      -k --cacert $${CONSUL_CACERT} --cert $${CONSUL_CLIENT_CERT} --key $${CONSUL_CLIENT_KEY} \\
-      $${CONSUL_ADDR}/v1/agent/members | jq '.' # Retrieve Consul members
-  $ curl \\
-      -X PUT \\
-      -d '{\"bar=baz\"}' \\
-      -k --cacert $${CONSUL_CACERT} --cert $${CONSUL_CLIENT_CERT} --key $${CONSUL_CLIENT_KEY} \\
-      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Write a KV
-  $ curl \\
-      -X GET \\
-      -k --cacert $${CONSUL_CACERT} --cert $${CONSUL_CLIENT_CERT} --key $${CONSUL_CLIENT_KEY} \\
-      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Read a KV"
-}
+${false == var.use_lb_cert ? "If you're making HTTP API requests to Consul from the Bastion host,\nthe below env var has been set for you.\n\n  $ export CONSUL_ADDR=http://127.0.0.1:8500\n\n  $ curl \\\n      -X GET \\\n      $${CONSUL_ADDR}/v1/agent/members | jq '.' # Retrieve Consul members\n  $ curl \\\n      -X PUT \\\n      -d '{\"bar=baz\"}' \\\n      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Write a KV\n  $ curl \\\n      -X GET \\\n      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Read a KV" : "If you're making HTTPS API requests to Consul from the Bastion host,\nthe below env vars have been set for you.\n\n  $ export CONSUL_ADDR=https://127.0.0.1:8080\n  $ export CONSUL_CACERT=/opt/consul/tls/consul-ca.crt\n  $ export CONSUL_CLIENT_CERT=/opt/consul/tls/consul.crt\n  $ export CONSUL_CLIENT_KEY=/opt/consul/tls/consul.key\n\n  $ curl \\\n      -X GET \\\n      -k --cacert $${CONSUL_CACERT} --cert $${CONSUL_CLIENT_CERT} --key $${CONSUL_CLIENT_KEY} \\\n      $${CONSUL_ADDR}/v1/agent/members | jq '.' # Retrieve Consul members\n  $ curl \\\n      -X PUT \\\n      -d '{\"bar=baz\"}' \\\n      -k --cacert $${CONSUL_CACERT} --cert $${CONSUL_CLIENT_CERT} --key $${CONSUL_CLIENT_KEY} \\\n      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Write a KV\n  $ curl \\\n      -X GET \\\n      -k --cacert $${CONSUL_CACERT} --cert $${CONSUL_CLIENT_CERT} --key $${CONSUL_CLIENT_KEY} \\\n      $${CONSUL_ADDR}/v1/kv/api | jq '.' # Read a KV"}
 
 # ------------------------------------------------------------------------------
 # ${var.name} HashiStack Vault Dev Guide Setup
@@ -90,7 +57,7 @@ up.
 below command to accomplish this automatically (we'll use Consul DNS moving
 forward once Vault is unsealed).
 
-  $ ssh -A ${lookup(var.users, var.os)}@$(curl http://127.0.0.1:8500/v1/agent/members | jq -M -r \
+  $ ssh -A ${var.users[var.os]}@$(curl http://127.0.0.1:8500/v1/agent/members | jq -M -r \
       '[.[] | select(.Name | contains ("${var.name}-hashistack")) | .Addr][0]')
 
 2.) Initialize Vault
@@ -121,7 +88,13 @@ host Vault CLI.
 You can interact with Vault using any of the
 CLI (https://www.vaultproject.io/docs/commands/index.html) or
 API (https://www.vaultproject.io/api/index.html) commands.
-${__builtin_StringToFloat(replace(replace(var.vault_version, "-ent", ""), ".", "")) >= 0100 || replace(var.vault_version, "-ent", "") != var.vault_version ? format("\nVault UI: %s%s %s\n\n%s", var.use_lb_cert ? "https://" : "http://", module.vault_lb_aws.vault_lb_dns, var.public ? "(Public)" : "(Internal)", var.public ? "The Vault nodes are in a public subnet with UI & SSH access open from the\ninternet. WARNING - DO NOT DO THIS IN PRODUCTION!\n" : "The Vault node(s) are in a private subnet, UI access can only be achieved inside\nthe network through a VPN.\n") : ""}
+${tonumber(tostring(replace(replace(var.vault_version, "-ent", ""), ".", ""))) >= 100 || replace(var.vault_version, "-ent", "") != var.vault_version ? format(
+"\nVault UI: %s%s %s\n\n%s",
+var.use_lb_cert ? "https://" : "http://",
+module.vault_lb_aws.vault_lb_dns,
+var.public ? "(Public)" : "(Internal)",
+var.public ? "The Vault nodes are in a public subnet with UI & SSH access open from the\ninternet. WARNING - DO NOT DO THIS IN PRODUCTION!\n" : "The Vault node(s) are in a private subnet, UI access can only be achieved inside\nthe network through a VPN.\n",
+) : ""}
 To start interacting with Vault, set your Vault token to authenticate requests.
 
 If using the "Vault Dev Guide", Vault is running in -dev mode & this has been set
@@ -140,40 +113,7 @@ Use the CLI to write and read a generic secret.
 Use the HTTP API with Consul DNS to write and read a generic secret with
 Vault's KV secret engine.
 
-${!var.use_lb_cert ?
-"If you're making HTTP API requests to Vault from the Bastion host,
-the below env var has been set for you.
-
-  $ export VAULT_ADDR=http://vault.service.vault:8200
-
-  $ curl \\
-      -H \"X-Vault-Token: $${VAULT_TOKEN}\" \\
-      -X POST \\
-      -d '{\"data\": {\"foo\":\"bar\"}}' \\
-      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Write a KV secret
-  $ curl \\
-      -H \"X-Vault-Token: $${VAULT_TOKEN}\" \\
-      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Read a KV secret"
-:
-"If you're making HTTPS API requests to Vault from the Bastion host,
-the below env vars have been set for you.
-
-  $ export VAULT_ADDR=https://vault.service.vault:8200
-  $ export VAULT_CACERT=/opt/vault/tls/vault-ca.crt
-  $ export VAULT_CLIENT_CERT=/opt/vault/tls/vault.crt
-  $ export VAULT_CLIENT_KEY=/opt/vault/tls/vault.key
-
-  $ curl \\
-      -H \"X-Vault-Token: $VAULT_TOKEN\" \\
-      -X POST \\
-      -d '{\"data\": {\"foo\":\"bar\"}}' \\
-      -k --cacert $${VAULT_CACERT} --cert $${VAULT_CLIENT_CERT} --key $${VAULT_CLIENT_KEY} \\
-      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Write a KV secret
-  $ curl \\
-      -H \"X-Vault-Token: $VAULT_TOKEN\" \\
-      -k --cacert $${VAULT_CACERT} --cert $${VAULT_CLIENT_CERT} --key $${VAULT_CLIENT_KEY} \\
-      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Read a KV secret"
-}
+${false == var.use_lb_cert ? "If you're making HTTP API requests to Vault from the Bastion host,\nthe below env var has been set for you.\n\n  $ export VAULT_ADDR=http://vault.service.vault:8200\n\n  $ curl \\\n      -H \"X-Vault-Token: $${VAULT_TOKEN}\" \\\n      -X POST \\\n      -d '{\"data\": {\"foo\":\"bar\"}}' \\\n      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Write a KV secret\n  $ curl \\\n      -H \"X-Vault-Token: $${VAULT_TOKEN}\" \\\n      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Read a KV secret" : "If you're making HTTPS API requests to Vault from the Bastion host,\nthe below env vars have been set for you.\n\n  $ export VAULT_ADDR=https://vault.service.vault:8200\n  $ export VAULT_CACERT=/opt/vault/tls/vault-ca.crt\n  $ export VAULT_CLIENT_CERT=/opt/vault/tls/vault.crt\n  $ export VAULT_CLIENT_KEY=/opt/vault/tls/vault.key\n\n  $ curl \\\n      -H \"X-Vault-Token: $VAULT_TOKEN\" \\\n      -X POST \\\n      -d '{\"data\": {\"foo\":\"bar\"}}' \\\n      -k --cacert $${VAULT_CACERT} --cert $${VAULT_CLIENT_CERT} --key $${VAULT_CLIENT_KEY} \\\n      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Write a KV secret\n  $ curl \\\n      -H \"X-Vault-Token: $VAULT_TOKEN\" \\\n      -k --cacert $${VAULT_CACERT} --cert $${VAULT_CLIENT_CERT} --key $${VAULT_CLIENT_KEY} \\\n      $${VAULT_ADDR}/v1/secret/data/api | jq '.' # Read a KV secret"}
 
 # ------------------------------------------------------------------------------
 # ${var.name} HashiStack Nomad
@@ -183,7 +123,13 @@ You can interact with Nomad using any of the CLI
 (https://www.nomadproject.io/docs/commands/index.html) or API
 (https://www.nomadproject.io/api/index.html) commands.
 
-${format("Nomad UI: %s%s %s\n\n%s", var.use_lb_cert ? "https://" : "http://", module.nomad_lb_aws.nomad_lb_dns, var.public ? "(Public)" : "(Internal)", var.public ? "The Nomad nodes are in a public subnet with UI & SSH access open from the\ninternet. WARNING - DO NOT DO THIS IN PRODUCTION!" : "The Nomad node(s) are in a private subnet, UI access can only be achieved inside\nthe network through a VPN.")}
+${format(
+"Nomad UI: %s%s %s\n\n%s",
+var.use_lb_cert ? "https://" : "http://",
+module.nomad_lb_aws.nomad_lb_dns,
+var.public ? "(Public)" : "(Internal)",
+var.public ? "The Nomad nodes are in a public subnet with UI & SSH access open from the\ninternet. WARNING - DO NOT DO THIS IN PRODUCTION!" : "The Nomad node(s) are in a private subnet, UI access can only be achieved inside\nthe network through a VPN.",
+)}
 
 Use the CLI to retrieve Nomad servers & clients, then deploy a Redis Docker
 container and check it's status.
@@ -203,135 +149,76 @@ Use the HTTP API to deploy a Redis Docker container.
 
   $ nomad run -output example.nomad > example.json # Convert job file to JSON
 
-${!var.use_lb_cert ?
-"If you're making HTTP API requests to Nomad from the Bastion host,
-the below env var has been set for you.
-
-  $ export NOMAD_ADDR=http://nomad.service.consul:4646
-
-  $ curl \\
-      -X POST \\
-      -d @example.json \\
-      $${NOMAD_ADDR}/v1/job/example/plan | jq '.' # Run a nomad plan
-  $ curl \\
-      -X POST \\
-      -d @example.json \\
-      $${NOMAD_ADDR}/v1/job/example | jq '.' # Run the example job
-  $ curl \\
-      -X GET \\
-      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is running
-  $ curl \\
-      -X GET \\
-      $${NOMAD_ADDR}/v1/job/example | jq '.' # Check job details
-  $ curl \\
-      -X DELETE \\
-      $${NOMAD_ADDR}/v1/job/example | jq '.' # Stop the example job
-  $ curl \\
-      -X GET \\
-      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is stopped"
-:
-"If you're making HTTPS API requests to Nomad from the Bastion host,
-the below env vars have been set for you.
-
-  $ export NOMAD_ADDR=https://nomad.service.consul:4646
-  $ export NOMAD_CACERT=/opt/nomad/tls/nomad-ca.crt
-  $ export NOMAD_CLIENT_CERT=/opt/nomad/tls/nomad.crt
-  $ export NOMAD_CLIENT_KEY=/opt/nomad/tls/nomad.key
-
-  $ curl \\
-      -X POST \\
-      -d @example.json \\
-      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\
-      $${NOMAD_ADDR}/v1/job/example/plan | jq '.' # Run a nomad plan on the example job
-  $ curl \\
-      -X POST \\
-      -d @example.json \\
-      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\
-      $${NOMAD_ADDR}/v1/job/example | jq '.' # Run the example job
-  $ curl \\
-      -X GET \\
-      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\
-      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is running
-  $ curl \\
-      -X GET \\
-      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\
-      $${NOMAD_ADDR}/v1/job/example | jq '.' # Check job details
-  $ curl \\
-      -X DELETE \\
-      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\
-      $${NOMAD_ADDR}/v1/job/example | jq '.' # Stop the example job
-  $ curl \\
-      -X GET \\
-      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\
-      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is stopped"
-}
+${false == var.use_lb_cert ? "If you're making HTTP API requests to Nomad from the Bastion host,\nthe below env var has been set for you.\n\n  $ export NOMAD_ADDR=http://nomad.service.consul:4646\n\n  $ curl \\\n      -X POST \\\n      -d @example.json \\\n      $${NOMAD_ADDR}/v1/job/example/plan | jq '.' # Run a nomad plan\n  $ curl \\\n      -X POST \\\n      -d @example.json \\\n      $${NOMAD_ADDR}/v1/job/example | jq '.' # Run the example job\n  $ curl \\\n      -X GET \\\n      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is running\n  $ curl \\\n      -X GET \\\n      $${NOMAD_ADDR}/v1/job/example | jq '.' # Check job details\n  $ curl \\\n      -X DELETE \\\n      $${NOMAD_ADDR}/v1/job/example | jq '.' # Stop the example job\n  $ curl \\\n      -X GET \\\n      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is stopped" : "If you're making HTTPS API requests to Nomad from the Bastion host,\nthe below env vars have been set for you.\n\n  $ export NOMAD_ADDR=https://nomad.service.consul:4646\n  $ export NOMAD_CACERT=/opt/nomad/tls/nomad-ca.crt\n  $ export NOMAD_CLIENT_CERT=/opt/nomad/tls/nomad.crt\n  $ export NOMAD_CLIENT_KEY=/opt/nomad/tls/nomad.key\n\n  $ curl \\\n      -X POST \\\n      -d @example.json \\\n      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\\n      $${NOMAD_ADDR}/v1/job/example/plan | jq '.' # Run a nomad plan on the example job\n  $ curl \\\n      -X POST \\\n      -d @example.json \\\n      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\\n      $${NOMAD_ADDR}/v1/job/example | jq '.' # Run the example job\n  $ curl \\\n      -X GET \\\n      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\\n      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is running\n  $ curl \\\n      -X GET \\\n      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\\n      $${NOMAD_ADDR}/v1/job/example | jq '.' # Check job details\n  $ curl \\\n      -X DELETE \\\n      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\\n      $${NOMAD_ADDR}/v1/job/example | jq '.' # Stop the example job\n  $ curl \\\n      -X GET \\\n      -k --cacert $${NOMAD_CACERT} --cert $${NOMAD_CLIENT_CERT} --key $${NOMAD_CLIENT_KEY} \\\n      $${NOMAD_ADDR}/v1/jobs | jq '.' # Check that the job is stopped"}
 README
-}
 
-output "hashistack_asg_id" {
-  value = "${element(concat(aws_autoscaling_group.hashistack.*.id, list("")), 0)}" # TODO: Workaround for issue #11210
-}
+    }
 
-output "consul_sg_id" {
-  value = "${module.consul_server_sg.consul_server_sg_id}"
-}
+    output "hashistack_asg_id" {
+      value = element(concat(aws_autoscaling_group.hashistack.*.id, [""]), 0) # TODO: Workaround for issue #11210
+    }
 
-output "consul_lb_sg_id" {
-  value = "${module.consul_lb_aws.consul_lb_sg_id}"
-}
+    output "consul_sg_id" {
+      value = module.consul_server_sg.consul_server_sg_id
+    }
 
-output "consul_tg_http_8500_arn" {
-  value = "${module.consul_lb_aws.consul_tg_http_8500_arn}"
-}
+    output "consul_lb_sg_id" {
+      value = module.consul_lb_aws.consul_lb_sg_id
+    }
 
-output "consul_tg_https_8080_arn" {
-  value = "${module.consul_lb_aws.consul_tg_https_8080_arn}"
-}
+    output "consul_tg_http_8500_arn" {
+      value = module.consul_lb_aws.consul_tg_http_8500_arn
+    }
 
-output "consul_lb_dns" {
-  value = "${module.consul_lb_aws.consul_lb_dns}"
-}
+    output "consul_tg_https_8080_arn" {
+      value = module.consul_lb_aws.consul_tg_https_8080_arn
+    }
 
-output "vault_sg_id" {
-  value = "${module.vault_server_sg.vault_server_sg_id}"
-}
+    output "consul_lb_dns" {
+      value = module.consul_lb_aws.consul_lb_dns
+    }
 
-output "vault_lb_sg_id" {
-  value = "${module.vault_lb_aws.vault_lb_sg_id}"
-}
+    output "vault_sg_id" {
+      value = module.vault_server_sg.vault_server_sg_id
+    }
 
-output "vault_tg_http_8200_arn" {
-  value = "${module.vault_lb_aws.vault_tg_http_8200_arn}"
-}
+    output "vault_lb_sg_id" {
+      value = module.vault_lb_aws.vault_lb_sg_id
+    }
 
-output "vault_tg_https_8200_arn" {
-  value = "${module.vault_lb_aws.vault_tg_https_8200_arn}"
-}
+    output "vault_tg_http_8200_arn" {
+      value = module.vault_lb_aws.vault_tg_http_8200_arn
+    }
 
-output "vault_lb_dns" {
-  value = "${module.vault_lb_aws.vault_lb_dns}"
-}
+    output "vault_tg_https_8200_arn" {
+      value = module.vault_lb_aws.vault_tg_https_8200_arn
+    }
 
-output "nomad_sg_id" {
-  value = "${module.nomad_server_sg.nomad_server_sg_id}"
-}
+    output "vault_lb_dns" {
+      value = module.vault_lb_aws.vault_lb_dns
+    }
 
-output "nomad_lb_sg_id" {
-  value = "${module.nomad_lb_aws.nomad_lb_sg_id}"
-}
+    output "nomad_sg_id" {
+      value = module.nomad_server_sg.nomad_server_sg_id
+    }
 
-output "nomad_tg_http_4646_arn" {
-  value = "${module.nomad_lb_aws.nomad_tg_http_4646_arn}"
-}
+    output "nomad_lb_sg_id" {
+      value = module.nomad_lb_aws.nomad_lb_sg_id
+    }
 
-output "nomad_tg_https_4646_arn" {
-  value = "${module.nomad_lb_aws.nomad_tg_https_4646_arn}"
-}
+    output "nomad_tg_http_4646_arn" {
+      value = module.nomad_lb_aws.nomad_tg_http_4646_arn
+    }
 
-output "nomad_lb_dns" {
-  value = "${module.nomad_lb_aws.nomad_lb_dns}"
-}
+    output "nomad_tg_https_4646_arn" {
+      value = module.nomad_lb_aws.nomad_tg_https_4646_arn
+    }
 
-output "hashistack_username" {
-  value = "${lookup(var.users, var.os)}"
-}
+    output "nomad_lb_dns" {
+      value = module.nomad_lb_aws.nomad_lb_dns
+    }
+
+    output "hashistack_username" {
+      value = var.users[var.os]
+    }
+
